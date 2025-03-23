12ac8205f24cc8efdc9ca025cafaf2c2ea5d787f
Alina Kim <alinakim707@gmail.com>
2025-03-23 22:45:00 +0500

    Update 1

--- Lab8/Racer/Game.py
+++ Lab8/Racer/Game.py
@@ -1,73 +1,86 @@
 import pygame, sys
 from pygame.locals import *
 import random, time
- 
-#Initializing 
+
+# Initializing
 pygame.init()
- 
-#Setting up FPS 
+
+# Setting up FPS
 FPS = 60
 FramePerSec = pygame.time.Clock()
- 
-#Creating colors
-BLUE  = (0, 0, 255)
-RED   = (255, 0, 0)
+
+# Creating colors
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
- 
-#Other Variables for use in the program
+
+# Other Variables for use in the program
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
 SPEED = 5
 SCORE = 0
- 
-#Setting up Fonts
+COIN_SCORE = 0  # Track coin points
+COIN_THRESHOLD = 10  # Points needed to increase speed
+
+# Setting up Fonts
 font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, BLACK)
- 
-#Create a white screen 
-DISPLAYSURF = pygame.display.set_mode((400,600))
+
+# Create a white screen
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
 # Load and play background music
-pygame.mixer.music.load("D:\\Pops\\Lab8\\Racer\\background_music.mp3")  # Make sure the path is correct
-pygame.mixer.music.play(-1, 0.0)  # Play music in a loop
+pygame.mixer.music.load("D:\\Pops\\Lab8\\Racer\\background_music.mp3")
+pygame.mixer.music.play(-1, 0.0)
+
+coin_sound = pygame.mixer.Sound(r"D:\Pops\Lab8\Racer\CoinSound.mp3")
+
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__() 
+        super().__init__()
         self.image = pygame.image.load("D:\\Pops\\Lab8\\Racer\\Enemy.png")
-        self.surf = pygame.Surface((42, 70))
-        self.rect = self.surf.get_rect(center = (random.randint(40,SCREEN_WIDTH-40), 0))
- 
+        self.rect = self.image.get_rect(center=(random.randint(40, SCREEN_WIDTH - 40), 0))
+
     def move(self):
         global SCORE
-        self.rect.move_ip(0,SPEED)
-        if (self.rect.top > 600):
+        self.rect.move_ip(0, SPEED)
+        if self.rect.top > SCREEN_HEIGHT:
             SCORE += 1
             self.rect.top = 0
             self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__() 
+        super().__init__()
         self.image = pygame.image.load("D:\\Pops\\Lab8\\Racer\\Player.png")
-        self.surf = pygame.Surface((40, 75))
-        self.rect = self.surf.get_rect(center = (160, 520))
-        
+        self.rect = self.image.get_rect(center=(160, 520))
+
     def move(self):
         pressed_keys = pygame.key.get_pressed()
-    
-        if self.rect.left > 0:
-              if pressed_keys[K_LEFT]:
-                  self.rect.move_ip(-5, 0)
-        if self.rect.right < SCREEN_WIDTH:        
-              if pressed_keys[K_RIGHT]:
-                  self.rect.move_ip(5, 0)
-                   
+        if self.rect.left > 0 and pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH and pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(r"D:\Pops\Lab8\Racer\Coin.png")
+        self.value = 1  
+        self.rect = self.image.get_rect(center=(random.randint(40, SCREEN_WIDTH - 40), 0))
+
+    def move(self):
+        self.rect.move_ip(0, SPEED)
+        if self.rect.top > SCREEN_HEIGHT:
+            self.kill()
+
+
 class Background():
     def __init__(self):
         self.bgimage = pygame.image.load('D:\\Pops\\Lab8\\Racer\\AnimatedStreet.png')
@@ -92,61 +105,71 @@ class Background():
     def render(self):
         DISPLAYSURF.blit(self.bgimage, (self.bgX1, self.bgY1))
         DISPLAYSURF.blit(self.bgimage, (self.bgX2, self.bgY2))
-         
-#Setting up Sprites        
+
+
+# Setting up Sprites
 P1 = Player()
 E1 = Enemy()
+coins = pygame.sprite.Group()
 
 back_ground = Background()
 
-#Creating Sprites Groups
 enemies = pygame.sprite.Group()
 enemies.add(E1)
+
 all_sprites = pygame.sprite.Group()
-all_sprites.add(P1)
-all_sprites.add(E1)
+all_sprites.add(P1, E1)
 
-#Adding a new User event 
-INC_SPEED = pygame.USEREVENT + 1
-pygame.time.set_timer(INC_SPEED, 1000)
 
-#Game Loop
+ADD_COIN = pygame.USEREVENT + 2
+pygame.time.set_timer(ADD_COIN, 2000)  # Spawn a coin every 2 seconds
+
+# Game Loop
 while True:
-    #Cycles through all occurring events   
     for event in pygame.event.get():
-        if event.type == INC_SPEED:
-              SPEED += 0.5     
+        if event.type == ADD_COIN:
+            new_coin = Coin()
+            coins.add(new_coin)
+            all_sprites.add(new_coin)
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
- 
+
     back_ground.update()
     back_ground.render()
- 
-    #DISPLAYSURF.blit(background, (0,0))
-    scores = font_small.render(str(SCORE), True, BLACK)
-    DISPLAYSURF.blit(scores, (10,10))
 
-    #Moves and Re-draws all Sprites
+
+    # Display scores
+    score_text = font_small.render(f"Score: {SCORE}", True, BLACK)
+    DISPLAYSURF.blit(score_text, (10, 10))
+
+    coin_text = font_small.render(f"Coins: {COIN_SCORE}", True, BLACK)
+    coin_text_rect = coin_text.get_rect(topright=(SCREEN_WIDTH - 10, 10))
+    DISPLAYSURF.blit(coin_text, coin_text_rect)
+
+    # Move and draw all sprites
     for entity in all_sprites:
         DISPLAYSURF.blit(entity.image, entity.rect)
         entity.move()
 
-    #To be run if collision occurs between Player and Enemy
+    # Check for coin collection
+    collected_coins = pygame.sprite.spritecollide(P1, coins, True)
+    for coin in collected_coins:
+        COIN_SCORE += coin.value
+        coin_sound.play()
+
+    # Collision detection
     if pygame.sprite.spritecollideany(P1, enemies):
-          pygame.mixer.Sound('D:\Pops\Lab8\Racer\crash.wav').play()
-          pygame.mixer.music.stop()
-          time.sleep(0.8)
-          pygame.mixer.Sound('D:\Pops\Lab8\Racer\GameOver.mp3').play()
-          DISPLAYSURF.fill(RED)
-          DISPLAYSURF.blit(game_over, (30,250))
-           
-          pygame.display.update()
-          for entity in all_sprites:
-                entity.kill() 
-          time.sleep(1.5)
-          pygame.quit()
-          sys.exit()        
-         
+        pygame.mixer.Sound("D:\\Pops\\Lab8\\Racer\\crash.wav").play()
+        pygame.mixer.music.stop()
+        time.sleep(0.8)
+        pygame.mixer.Sound("D:\\Pops\\Lab8\\Racer\\GameOver.mp3").play()
+        DISPLAYSURF.fill(RED)
+        DISPLAYSURF.blit(game_over, (30, 250))
+        pygame.display.update()
+        time.sleep(1.5)
+        pygame.quit()
+        sys.exit()
+
     pygame.display.update()
     FramePerSec.tick(FPS)
